e492505e3a7a72fcb96880aca93cee5f4e71c9a3
Alice <darondeau.alice@gmail.com>
2022-12-08 16:37:06 +0100

    mode à deux joueurs créé, reste la gestion de sortie des conditions à faire

--- allumettes.py
+++ allumettes.py
@@ -8,11 +8,12 @@ def removeChoice (leftover, choicePlayer) :
 
     #on vérifie que le choix est compris entre 1 et 6
     if not 1 <= choicePlayer <= 6:
-        print("Le nombre d'allumettes à retirer doit être compris entre 1 et 6")
-        
+        print("Le nombre d'allumettes à retirer doit être compris entre 1 et 6.")
+        choicePlayer = input("Choose a number between 1 and 6: ")
+
     #on vérifie que le choix n'excède pas la pioche 
     if choicePlayer > leftover:
-        print("Le nombre d'allumettes à retirer ne doit pas pas être strictement supérieur à la pioche")
+        print("Le nombre d'allumettes à retirer ne doit pas pas être strictement supérieur à la pioche.")
         
     #on actualise la pioche avec le choix du joueur 
     else:
@@ -33,6 +34,11 @@ def eachTurn (remaining) :
         remaining = removeChoice(remaining, choicePlayer1)
         print(remaining) 
 
+        #je contrôle si le jeu est terminé 
+        if remaining == 0 : 
+            print("Player 1, you won !")
+            return
+
         #je créé le tour du joueur 2 
         choicePlayer2 = input("Player 2, choose a number between 1 and 6: ")
         choicePlayer2 = int(choicePlayer2)
@@ -41,8 +47,11 @@ def eachTurn (remaining) :
         remaining = removeChoice(remaining, choicePlayer2)
         print(remaining) 
 
-    #une fois qu'on sort de la boucle, le jeu est terminé 
-    print("Game is over")
+        #je contrôle si le jeu est terminé 
+        if remaining == 0 : 
+            print("Player 2, you won !")
+            return
+
 
 #appel de la fonction qui lance le jeu 
 eachTurn(50)
